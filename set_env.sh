f52e0f61a7f6e8b9976429eea6d633f5c3b7b976
DawitSisayy <dawitsisay42@gmail.com>
2022-10-07 09:40:14 +0300

    first commit

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=mydb-instance.cx7zjiy4wdv7.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=dawitdb
+export AWS_BUCKET=udagram-245378921898-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
